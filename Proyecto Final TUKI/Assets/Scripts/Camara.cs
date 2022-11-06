f59b0d9dc8ebd9e9500679b9db4870d76a7aa006
JuanVillagra <villagrajuan99@gmail.com>
2022-11-06 13:21:44 -0300

    Arreglado las colisiones y añadiendo mas enemigos

--- Proyecto Final TUKI/Assets/Scripts/Camara.cs
+++ Proyecto Final TUKI/Assets/Scripts/Camara.cs
@@ -10,7 +10,7 @@ public class Camara : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.Find("Player").transform;
+        target = GameObject.Find("PlayerFinal").transform;
     }
 
     // Update is called once per frame
